2abdc7c08b3219fb305e38e61135085d8be5f7fe
Jeff Benton <sikletz@gmail.com>
2019-05-06 13:34:39 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -9,7 +9,7 @@ class App < Sinatra::Base
   
   post '/pig_latin' do
     piglatinizer = PigLatinizer.new
-    @str = piglatinizer.piglatinize(params[:user_input])
+    @str = piglatinizer.piglatinize(params[:user_phrase])
     erb :pig_latin
   end
   
